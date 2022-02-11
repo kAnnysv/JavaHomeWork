fda8c4117addf54099c8a9cd194e0c1e9b700b2f
valss <valssss@yandex.ru>
2022-02-11 13:54:07 +0300

    Застрял на JSlider

---
+++ hw7/GameField.java
@@ -0,0 +1,15 @@
+package hw7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+
+    GameField() {
+        setBackground(new Color(22, 203, 131, 107));
+    }
+
+
+
+
+}
